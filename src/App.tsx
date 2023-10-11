5a99dea8bda47f2dabe529a195b865884b1ed8d2
Jotape <juanpa1999a@gmail.com>
2023-10-11 19:18:45 -0300

    Add Navbar and Routes, PokeFinder hasn't 'id' yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./views/Home";
+import PokeFinder from "./views/PokeFinder";
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/pokemon" element={<PokeFinder/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
