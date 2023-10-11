76929d0bf6490b1480245f2dbac1c59d5fb78a18
Jotape <juanpa1999a@gmail.com>
2023-10-11 19:14:50 -0300

    Add isActive with 2 string classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     
+    const setActiveClass = ({ isActive }: {isActive:boolean}):string => (isActive ? "active" : "not-active");
     
     return (
         <div>
