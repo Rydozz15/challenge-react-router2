b62293c4bf8e6b81a191a95afeda209a48cec39a
Jotape <juanpa1999a@gmail.com>
2023-10-12 01:36:45 -0300

    Add pseudo-card render

--- src/views/PokeFinder.tsx
+++ src/views/PokeFinder.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { PokeContext } from "../context/PokeContext";
+import { PokemonApiObject } from "../@types/poke";
 
 const PokeFinder = () => {
     const { storedPokemon } = useParams();
@@ -14,6 +15,18 @@ const PokeFinder = () => {
     console.log(pokemon)
     return(
         <div>
+            {pokemon == "https://pokeapi.co/api/v2/pokemon/pikachu" 
+            ? ""
+            :<section>
+                <h1 key={pokemon.name}>{pokemon.name[0].toUpperCase()+pokemon.name.substring(1)}</h1>
+                <img src={`${pokemon.sprites.other["official-artwork"]["front_default"]}`} alt="" />
+                <ul>
+                    {pokemon.stats.map((element:PokemonApiObject) => (
+                        <li key={element.stat.name}>{element.stat.name} : {element["base_stat"]}</li>
+                    ))}
+                    <li>type : {pokemon.types[0].type.name}</li>
+                </ul>
+            </section>}
         </div>
     )
 }
