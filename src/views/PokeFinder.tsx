d1cfdb09a4893d9b02cae4522ba0713db3c56b2e
Jotape <juanpa1999a@gmail.com>
2023-10-12 01:36:16 -0300

    Add URL updater on new page through params

--- src/views/PokeFinder.tsx
+++ src/views/PokeFinder.tsx
@@ -1,7 +1,20 @@
-const PokeFinder = () => {
+import { useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { PokeContext } from "../context/PokeContext";
 
+const PokeFinder = () => {
+    const { storedPokemon } = useParams();
+    const API_URL = `https://pokeapi.co/api/v2/pokemon/${storedPokemon}`;
+    const  {setPokemonUrl,pokemon} = useContext(PokeContext)
+
+    useEffect(() => {
+        setPokemonUrl(API_URL);
+      }, []);
+
+    console.log(pokemon)
     return(
-        <div>Work in progress: There'll be a pokemon in the future here</div>
+        <div>
+        </div>
     )
 }
 
