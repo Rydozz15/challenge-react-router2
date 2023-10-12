9c40febdcfc9d2f55917959ba655c8b105541c8b
Jotape <juanpa1999a@gmail.com>
2023-10-12 00:10:42 -0300

    Add Context API and Image render

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { PokeContext } from "../context/PokeContext";
+import { PokemonApiObject } from "../@types/poke";
+
 const Home = () => {
+    const  {pokemon, setPokemonUrl} = useContext(PokeContext)
 
     return(
-        <div>Work in progress: There'll be a welcoming message</div>
+        <article>
+            {pokemon == null 
+            ? ""
+            :<section>
+                <h1>Welcome Pokemon Master</h1>
+                <img src={`${pokemon.sprites.other["official-artwork"]["front_default"]}`} alt="" />
+            </section>}
+        </article>
     );
 };
 
